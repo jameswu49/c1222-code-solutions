f8d4efa2869525408ce37b9238d312bf7d5e8392
Jimmy Wu <jameswu49@gmail.com>
2022-12-12 18:55:27 +0000

    Completed javascript-operators-and-expressions exercise

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 3;
+var height = 4;
+var area = width * height;
+console.log('value of', area);
+console.log('type of area', typeof area);
+
+var bill = 20;
+var payment = 40;
+var change = bill - payment;
+console.log('value of', change);
+console.log('type of', typeof change);
+
+var quizzes = 20;
+var midterm = 50;
+var final = 90;
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of', grade);
+console.log('type of', typeof grade);
+
+var firstName = 'Jimmy ';
+var lastName = 'Wu';
+var fullName = firstName + lastName;
+console.log('value of', fullName);
+console.log('type of', typeof fullName);
+
+var pH = 15;
+var isAcidic = pH < 7;
+console.log('value of', isAcidic);
+console.log('type of', typeof isAcidic);
+
+var headCount = 30;
+var isSparta = headCount === 300;
+console.log('value of', isSparta);
+console.log('type of', typeof isSparta);
+
+var motto = fullName;
+motto = fullName + ' is the GOAT';
+console.log('value of', motto);
+console.log('type of', typeof motto);
