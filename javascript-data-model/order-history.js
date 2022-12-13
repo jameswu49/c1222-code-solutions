c62a4630ff7d58b50785be61cfbd2ccdaa1327c2
Jimmy Wu <jameswu49@gmail.com>
2022-12-13 18:19:27 +0000

    Completed javascript-data-model exercise

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,78 @@
+var orderHistory = [
+  {
+    orderMonth: 'August 4, 2020',
+    total: 34.00,
+    shipTo: 'JS Masher',
+    orderNumber: '114-3941689-8772232',
+    dateDelivered: 'August 8, 2020',
+    packageHandled: null,
+    item: 'book',
+    itemName: null,
+    title: 'Javascript for Impatient programmers',
+    author: 'Rauschmayer, Dr. Axel',
+    returnWindow: 'Sep 7, 2020',
+    price: 31.55
+  },
+  {
+    orderMonth: 'July 19, 2020',
+    total: 44.53,
+    shipTo: 'JS Masher',
+    orderNumber: '113-9984268-1280257',
+    dateDelivered: 'Jul 20, 2020',
+    packageHandled: 'Your package was delivered. It was handed directly to a resident.',
+    item: 'book',
+    itemName: null,
+    title: 'The Timeless Way of Building',
+    author: 'Alexander, Christopher',
+    returnWindow: 'Aug 19, 2020',
+    price: 41.33
+  },
+  {
+    orderPlaced: 'July 4, 2020',
+    total: 17.22,
+    shipTo: 'JS Masher',
+    orderNumber: '114-2875557-9059409',
+    dateDelivered: 'Jul 7, 2020',
+    packageHandled: 'Your package was delivered. It was handed directly to a resident.',
+    item: 'electronic',
+    itemName: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+    title: null,
+    author: null,
+    returnWindow: 'Aug 5th, 2020',
+    price: 15.98
+  },
+  [
+    {
+      orderPlaced: 'July 3, 2020',
+      total: 138.93,
+      shipTo: 'JS Masher',
+      orderNumber: '113-2883177-2648248',
+      dateDelivered: 'Jul 5, 2020',
+      packageHandled: null,
+      item: 'electronic',
+      itemName: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+      title: null,
+      author: null,
+      returnWindow: 'Aug 4, 2020',
+      price: 94.95
+    },
+    {
+      orderPlaced: 'July 3, 2020',
+      total: 138.93,
+      shipTo: 'JS Masher',
+      orderNumber: '113-2883177-2648248',
+      dateDelivered: 'Jul 5, 2020',
+      packageHandled: null,
+      item: 'book',
+      itemName: null,
+      title: 'Faroult, Stephane',
+      returnWindow: 'Aug 4, 2020',
+      price: 33.99
+    }
+  ]
+];
+
+console.log('value of orderHistory[0]', orderHistory[0]);
+console.log('value of orderHistory[1]', orderHistory[1]);
+console.log('value of orderHistory[2]', orderHistory[2]);
+console.log('value of orderHistory[3]', orderHistory[3]);
