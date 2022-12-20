1c2131f16209e34345c528af2ef5c6492380d92c
Jimmy Wu <jameswu49@gmail.com>
2022-12-20 22:11:53 +0000

    in progress

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,1 +1,26 @@
 /* exported chunk */
+
+// input: Array, size
+
+// output: return a new array with subarrays that has a length equal to the size input
+// Remaining elements are put into the final array
+
+// use slice method
+//   - index starts at 0 and end point is size input
+
+// function chunk(array, size) {
+//   var arr = [];
+//   for (let i = 0; i < array.length; i++) {
+//     arr.push(array.slice(i, size));
+//   }
+//   console.log(arr);
+// }
+
+// - slice array
+// - push array into new array
+
+// how to make it repeat ?
+//  -create for loop
+//  - loop over how many times?
+
+// - return array at the end
--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -4,10 +4,9 @@
 
 // output: return a new array that contains the elements of array before last count elements
 
+// return and use slice method
+//   - set start of index to 0 and end to count multiplied by -1
+
 function dropRight(array, count) {
-  var arr = [];
-  for (var i = 0; i < array.length; i++) {
-    arr.push(array[i]);
-  }
-  return arr;
+  return array.slice(0, count * -1);
 }
