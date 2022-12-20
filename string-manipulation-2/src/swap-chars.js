18b53c2245d1ba7111d0921b171c71a5be8ccd5f
Jimmy Wu <jameswu49@gmail.com>
2022-12-20 00:08:45 +0000

    in progress

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -4,28 +4,33 @@
 
 // output: swap the first index position with the second index position of the string
 
-// target index of string
-// replace string's first index with second index and assign to a new variable
-// replace string's second index with first index and assign to a new variable
-// replace first new variable's second index with
+// create new variable and assign string to
+// replace newString's first index with second index and assign to a new variable
+// replace new variable's second index with first index of the newStrings first index and assign to a new variable
+// return final new variable
+
+// function swapChars(firstIndex, secondIndex, string) {
+//   var newString = string;
+//   var firstSwap = newString.replace(newString[firstIndex], newString[secondIndex]);
+//   var secondSwap = firstSwap.replace(firstSwap[secondIndex], newString[firstIndex]);
+//   return secondSwap;
+// }
+
+// create empty string variable
+// create for loop to loop over each letter
+// create if statement
+//   - if index of word is same as firstindex then assign secondindex to it
+//   - else if index of word is secondindex then assign firstindex to it
+// return variable
 
 function swapChars(firstIndex, secondIndex, string) {
-  var split = string.split('');
-  var firstSwap = string.replace(string[firstIndex], split[secondIndex]);
-  var secondSwap = firstSwap.replace(firstSwap[secondIndex], split[firstIndex]);
-  return secondSwap;
+  var split = string.split();
+  for (let i = 0; i < split.length; i++) {
+    if (split[i] === split[firstIndex]) {
+      split[i] = string[firstIndex];
+    } else if (split[i] === split[secondIndex]) {
+      split[i] = split[secondIndex];
+    }
+  }
+  return split;
 }
-
-// swap first index with second index of string
-
-// oodash
-
-// lldash
-
-// first = oodash
-
-// second = lldash
-
-// third = oodash
-
-// oldash
