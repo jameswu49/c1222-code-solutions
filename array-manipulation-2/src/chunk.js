8e2562f43f16e4e52e3f6e991f9f5be30e7c5757
Jimmy Wu <jameswu49@gmail.com>
2022-12-24 22:16:00 +0000

    in progress

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -8,19 +8,10 @@
 // use slice method
 //   - index starts at 0 and end point is size input
 
-// function chunk(array, size) {
-//   var arr = [];
-//   for (let i = 0; i < array.length; i++) {
-//     arr.push(array.slice(i, size));
-//   }
-//   console.log(arr);
-// }
-
-// - slice array
-// - push array into new array
-
-// how to make it repeat ?
-//  -create for loop
-//  - loop over how many times?
-
-// - return array at the end
+function chunk(array, size) {
+  var arr = [];
+  for (var i = 0; i < array.length; i++) {
+    arr.push(array.slice(i, size));
+  }
+  // console.log(arr);
+}
