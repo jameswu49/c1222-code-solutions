43c1ce252546cfea097e07f24c8ae6e2741c503d
Jimmy Wu <jameswu49@gmail.com>
2022-12-26 22:35:27 +0000

    Completed array-manipulation-2

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -5,13 +5,29 @@
 // output: return a new array with subarrays that has a length equal to the size input
 // Remaining elements are put into the final array
 
-// use slice method
-//   - index starts at 0 and end point is size input
+// create an empty array and assign to variable
+// use slice method to get values
+//   - create end variable and assign to size input as the end for slice
+// create for loop to iterate a certain number of times
+//   - set i to 0
+//   - stop loop when i is the length of array
+//   - increment i by size and end varible by size
+//     - i is starting point of slice index and will increment by size
+//     - end is ending point and will increment by size
+//   - create if statement
+//     - if i is less than the length of array, slice and push values into the array
+//     - else, stop the loop
+// return array variable
 
 function chunk(array, size) {
   var arr = [];
-  for (var i = 0; i < array.length; i++) {
-    arr.push(array.slice(i, size));
+  var end = size;
+  for (var i = 0; i < array.length; i += size, end += size) {
+    if (i < array.length) {
+      arr.push(array.slice(i, end));
+    } else {
+      break;
+    }
   }
-  // console.log(arr);
+  return arr;
 }
