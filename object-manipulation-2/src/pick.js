3b7052f1503ccf5e25c37fbe0f4b78eab15fd42d
Jimmy Wu <jameswu49@gmail.com>
2022-12-27 05:56:23 +0000

    in progress

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -12,11 +12,26 @@
 //     - dot notation key into the object
 // return object
 
-function pick(source, keys) {
-  var newKeys = Object.keys(source);
-  for (var i = 0; i < newKeys.length; i++) {
-    if (newKeys[i] === keys[i]) {
-      // console.log(keys[i]);
-    }
-  }
-}
+// function pick(source, keys) {
+//   var object = {};
+//   var objKeys = Object.keys(source);
+//   for (var i = 0; i < keys.length; i++) {
+//     if (objKeys.includes(keys[i])) {
+//       object[keys[i]] = source[keys[i]];
+//     }
+//     console.log(object);
+//   }
+//   return object;
+// }
+
+// create empty object and assign to variable
+// target source keys and assign to variable
+// create for loop to loop through the keys input
+//   - start at 0, stop when less than keys input length, and iterate by 1
+// create if statement
+//   - if first element in the keys input array matches first key in the source object,
+//   assign the first key and value of the source input to the object variable
+//   - else compared element to every other value
+
+//   how to loop over every other value?
+//   - use while loop and nested if statements?
