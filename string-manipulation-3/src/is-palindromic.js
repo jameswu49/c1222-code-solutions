9cf7e5a39f1239f2a8f7028007386efb8756cf3b
Jimmy Wu <jameswu49@gmail.com>
2023-01-04 19:59:15 +0000

    in progress

---
+++ string-manipulation-3/src/is-palindromic.js
@@ -0,0 +1,16 @@
+/* exported isPalindromic */
+
+// input: string
+
+// output: Return a boolean on whether string input is a palindrome
+
+// split string into an array
+// reverse array
+// join the array back into a string
+// compared new string with string input and check whether they match
+
+// function isPalindromic(string) {
+//   var newString = string.split('');
+//   var reversed = newString.reverse();
+//   // console.log(reversed);
+// }
